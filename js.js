4eca8cea93dc83e36fdc38da2ba7e7f3888673a7
Anton <strang999@gmail.com>
2019-04-13 15:42:09 +0300

    eto nash pervii commit

---
+++ js.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+
+console.log(4/0);
+console.log('string'/9);
+
+let something;
+console.log(something);
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0])
+
+//alert('hello word');
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let answer = +prompt("est li va", "da");
+
+// console.log(typeof(answer));
+
+// console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+    // incr++;
+    // decr--;
+
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(5%2);
+    console.log(5%2);
+
+    console.log("2" === 2);
+
+    let ischecked = true,
+        isClose = true;
+
+    console.log(ischecked && isClose);
