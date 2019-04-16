ffb4235e0a46626370b00a3504a834c2e55dadbc
Anton <strang999@gmail.com>
2019-04-16 13:25:00 +0300

    domashnee zadanie 2

---
+++ lesson2_dz/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+let i = 0;
+do {
+
+    if  ((typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null
+    && a != '' && b != '' && a.length < 50) {
+    console.log("done");
+    i++;
+    appData.expenses [a] = b;
+    } else {
+
+    } }
+    while (i < 2);
